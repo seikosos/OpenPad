3b6abc44349eb8cfd23b654adaca19b88c7a05e0
seikosos <dertfertde@gmail.com>
2026-03-12 13:47:50 +0100

    add remaining buttons

--- OpenPad.cpp
+++ OpenPad.cpp
@@ -16,11 +16,18 @@ struct ButtonConfig {
 	int Button;
 };
 
+// LSP = Left Stick Click, RSP = Right Stick Click
 const ButtonConfig Buttons[] = {
     {4, 'A', BUTTON_1},
     {5, 'B', BUTTON_2},
 	{6, 'X', BUTTON_3},
     {7, 'Y', BUTTON_4},
+	{15, 'L', BUTTON_5},
+    {16, 'R', BUTTON_6},
+	{17, 'ZL', BUTTON_7},
+    {18, 'ZR', BUTTON_8},
+	{8, 'LSC', BUTTON_9},
+    {3, 'RSC', BUTTON_10},
 };
 
 void setup() {
