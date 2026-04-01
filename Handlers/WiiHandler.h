591c349fccf2fb4029a9935a37b34200d682c7c7
seikosos <dertfertde@gmail.com>
2026-04-01 20:16:52 +0200

    clean wii handler from unneeded code

--- Handlers/WiiHandler.h
+++ Handlers/WiiHandler.h
@@ -1,13 +1,8 @@
 #pragma once
 #include "Handler.h"
-#include <BleGamepad.h>
 
 class WiiHandler : public Handler {
 public:
-    BleGamepadConfiguration* config;
-    BleGamepad* bleGamepad;
-    std::map<String, Bounce> *debouncers;
-
     void MainButtons() override;
     void SpecialButton() override;
     void Init();
